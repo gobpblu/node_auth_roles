44edc472b7503fcd5a3ae5c06e7a8cd26f3cfb25
Max Abramov <gobpo2002@gmail.com>
2026-02-13 06:26:43 +0600

    Add authMiddleware and roleMiddleware

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "NODE_AUTH_ROLES_SECRET_KEY"
+}
--- index.js
+++ index.js
@@ -8,12 +8,9 @@ const app = express();
 app.use(express.json());
 app.use("/auth", authRouter)
 
-const uri = "mongodb+srv://gobpo2002:Qwer1234@Cluster0.joxk3mq.mongodb.net/auth_roles?retryWrites=true&w=majority";
-
-
 const start = async () => {
     try {
-        await mongoose.connect(uri)
+        await mongoose.connect("mongodb+srv://gobpo2002:qwer1234@cluster0.joxk3mq.mongodb.net/auth_roles?appName=Cluster0")
         app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
     } catch (e) {
         console.log(e)
